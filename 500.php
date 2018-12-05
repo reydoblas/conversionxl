a28440be1083d1cc701366818f05ce72913ee6b9
Rey Doblas <rey.doblas@auza.net>
2018-12-05 22:12:02 +0800

    Integrate PWA 500 and offline template files to Mythic

---
+++ 500.php
@@ -0,0 +1,4 @@
+<?php
+
+// Load 500.php template.
+Hybrid\View\display( '500', Hybrid\Template\hierarchy() );
---
+++ offline.php
@@ -0,0 +1,4 @@
+<?php
+
+// Load offline.php template.
+Hybrid\View\display( 'offline', Hybrid\Template\hierarchy() );
